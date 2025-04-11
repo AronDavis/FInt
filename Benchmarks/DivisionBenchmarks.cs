75f99c62a38bced2b2f7a1cf463327e944646e37
Aron Davis <arontd@gmail.com>
2025-04-11 15:39:45 -0400

    Added benchmarks

---
+++ Benchmarks/DivisionBenchmarks.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    public class DivisionBenchmarks
+    {
+        private FInt _a = new FInt(100);
+        private FInt _b = new FInt(200);
+
+        private long _al = 100;
+        private long _bl = 200;
+
+        [Benchmark(Baseline = true)]
+        public long DivisionLong() => _al / _bl;
+
+        [Benchmark()]
+        public FInt Division() => _a / _b;
+    }
+}
